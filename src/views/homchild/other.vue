803a53e46fa619a934334e181d55f6531975f5c5
sitkinYip <ysxdyx@gmail.com>
2020-05-05 22:45:55 +0800

    sitkin:条形图和音乐插件

--- src/views/homchild/other.vue
+++ src/views/homchild/other.vue
@@ -10,14 +10,26 @@
           7:00-10:00为更新高峰且数据更新有稍许延迟，数据若滞后敬请谅解
         </template>
         <hr class="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p></p>
+        <!-- ------------------Aplayer------ -->
+        <ncovplayer></ncovplayer>
+        <!-- ----------------------------0-- -->
       </b-jumbotron>
 
-      <h2>实时详细数据</h2>
-      <b-table id="table-transition-example" :items="countryitems"></b-table>
+<!-- ------------------------------------------------- -->
 
-
-      <!-- <div id="myChart" ref="myChart" style="width: 100%;height: 600px;"></div> -->
+      <div class="table-style">
+        <h2>国内疫情实时详细数据</h2>
+        <b-table id="table-transition-example" :items="countryitems" :busy="isBusy" hover small caption-top responsive>
+          <template v-slot:table-busy>
+            <div class="text-center text-danger my-2">
+              <b-spinner class="align-middle"></b-spinner>
+              <strong>Loading...</strong>
+            </div>
+          </template>
+        </b-table>
+      </div>
+      <div id="myChart" ref="myChart" style="width: 100%;height: 50rem;margin-bottom: 3.2rem;"></div>
     </div>
   </div>
 </template>
@@ -39,8 +51,10 @@ export default{
               死亡:32,
               重症:32,
               _rowVariant: 'danger'
-            }
-          ]
+            },
+          ],
+          isBusy: true,
+          othersource:[["疫情地区Top5"],["疑似"],["累计确诊"],["痊愈数"],["死亡数"]]
       }
   },
   methods:{
@@ -55,12 +69,13 @@ export default{
             params: {
               key: '7e83a892f6a95b46d88ba6c7ffc242b1'
             }}).then(res=>{
-
                 if(res.data.code == 200){
+                  this.isBusy = !this.isBusy
                   let Tabledata=[]
                     for(let i in res.data.newslist){
                         let CountrtTableObj = {};
-                        CountrtTableObj.地区=res.data.newslist[i].provinceName
+                        CountrtTableObj.疫情地区=res.data.newslist[i].provinceName
+                        CountrtTableObj.现有=res.data.newslist[i].currentConfirmedCount
                         CountrtTableObj.累计=res.data.newslist[i].confirmedCount
                         CountrtTableObj.疑似=res.data.newslist[i].suspectedCount
                         CountrtTableObj.治愈=res.data.newslist[i].curedCount
@@ -68,20 +83,30 @@ export default{
                         Tabledata.push(CountrtTableObj)
                     }
                     this.countryitems=Tabledata
-
-                    console.log(res.data)
-
                 }
+               // -----------------------柱状图数据-------------
+                for (let i = 0; i < mapdata.dataset.source.length ; i++) {
+                  this.othersource[0].push(this.countryitems[i].疫情地区);
+                  this.othersource[1].push(this.countryitems[i].疑似)
+                  this.othersource[2].push(this.countryitems[i].累计)
+                  this.othersource[3].push(this.countryitems[i].治愈)
+                  this.othersource[4].push(this.countryitems[i].死亡)
+                }
+                 // console.log(this.othersource)
+                mapdata.dataset.source=this.othersource
+                // ------------------------
+                this.mapEchartsInit();
             })
-      }
 
+      },
 },
 created() {
-  this.getcontinentdata()
+
 },
   mounted(){
-    // this.mapEchartsInit();
-}
+this.getcontinentdata()
+},
+
 }
 </script>
 
@@ -92,4 +117,8 @@ created() {
   margin-top: 1rem;
   height:  calc(100% - 3.2rem);
 }
+/* .table-style{
+  width: 70rem;
+  margin: 0 auto;
+} */
 </style>
